da54bc3ad030ac5487e6648742cf14e6ee6caed3
DDunDDang <thdwns0331@gmail.com>
2023-07-29 23:13:34 +0900

    feat: 회원가입, 정보 등록

--- src/main/java/Moyoung/Server/exception/ExceptionCode.java
+++ src/main/java/Moyoung/Server/exception/ExceptionCode.java
@@ -3,6 +3,8 @@ package Moyoung.Server.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
+    MEMBER_NOT_FOUND(404, "회원을 찾을 수 없습니다"),
+    MEMBER_ID_EXISTS(409, "현재 사용중인 아이디 입니다."),
     UNAUTHORIZED(404, "권한이 없습니다" );
     @Getter
     private int status;
--- src/main/java/Moyoung/Server/member/mapper/MemberMapper.java
+++ src/main/java/Moyoung/Server/member/mapper/MemberMapper.java
@@ -1,4 +1,30 @@
 package Moyoung.Server.member.mapper;
 
+import Moyoung.Server.member.dto.MemberDto;
+import Moyoung.Server.member.entity.Member;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface MemberMapper {
+    Member postToMember(MemberDto.Post requestBody);
+    default Member infoToMember(MemberDto.Info requestBody) {
+        int age = requestBody.getAge();
+        Member member = new Member();
+        member.setDisplayName(requestBody.getDisplayName());
+        member.setGender(requestBody.isGender());
+
+        switch (age) {
+            case 1:
+                member.setAge(Member.Age.TEENAGER);
+                break;
+            case 2:
+                member.setAge(Member.Age.TWENTIES);
+                break;
+            case 3:
+                member.setAge(Member.Age.THIRTIES);
+                break;
+        }
+
+        return member;
+    }
 }
