f5595c8a67fad928fdd61205d21f9584501c3a19
DDunDDang <thdwns0331@gmail.com>
2023-08-02 22:14:28 +0900

    feat: 게시글 찾기

--- src/main/java/Moyoung/Server/recruitingarticle/service/RecruitingArticleService.java
+++ src/main/java/Moyoung/Server/recruitingarticle/service/RecruitingArticleService.java
@@ -1,11 +1,24 @@
 package Moyoung.Server.recruitingarticle.service;
 
+import Moyoung.Server.exception.BusinessLogicException;
+import Moyoung.Server.exception.ExceptionCode;
+import Moyoung.Server.recruitingarticle.entity.RecruitingArticle;
 import Moyoung.Server.recruitingarticle.repository.RecruitingArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class RecruitingArticleService {
     private final RecruitingArticleRepository recruitingArticleRepository;
+
+    public RecruitingArticle findVerifiedRecruitingArticle(long recruitingArticleId) {
+        Optional<RecruitingArticle> optionalRecruitingArticle = recruitingArticleRepository.findById(recruitingArticleId);
+        RecruitingArticle recruitingArticle = optionalRecruitingArticle.orElseThrow(() ->
+                new BusinessLogicException(ExceptionCode.RECRUIT_ARTICLE_NOT_FOUND));
+
+        return recruitingArticle;
+    }
 }
