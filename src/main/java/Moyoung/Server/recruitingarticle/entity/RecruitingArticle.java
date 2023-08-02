84f94f22d5c8e21f7e54bb80457a2eadd8d5af91
DDunDDang <thdwns0331@gmail.com>
2023-08-02 22:13:40 +0900

    feat: 참여자 추가

--- src/main/java/Moyoung/Server/recruitingarticle/entity/RecruitingArticle.java
+++ src/main/java/Moyoung/Server/recruitingarticle/entity/RecruitingArticle.java
@@ -2,9 +2,14 @@ package Moyoung.Server.recruitingarticle.entity;
 
 import Moyoung.Server.member.entity.Member;
 import Moyoung.Server.runningtime.entity.RunningTime;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
+@Getter
+@Setter
 @Entity
 public class RecruitingArticle {
     @Id
@@ -14,7 +19,13 @@ public class RecruitingArticle {
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
+    @OneToMany
+    private List<Member> participants;
     @ManyToOne
     @JoinColumn(name = "running_time_id")
     private RunningTime runningTime;
+
+    public void addParticipant(Member member) {
+        this.participants.add(member);
+    }
 }
