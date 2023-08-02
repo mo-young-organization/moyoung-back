997355f199da080906d9eba4c9f948465d72fcd7
DDunDDang <thdwns0331@gmail.com>
2023-08-02 22:15:10 +0900

    feat: 채팅 보내기

--- src/main/java/Moyoung/Server/chat/dto/ChatDto.java
+++ src/main/java/Moyoung/Server/chat/dto/ChatDto.java
@@ -1,4 +1,10 @@
 package Moyoung.Server.chat.dto;
 
+import lombok.Getter;
+
 public class ChatDto {
+    @Getter
+    public static class Post {
+        private String content;
+    }
 }
--- src/main/java/Moyoung/Server/chat/mapper/ChatMapper.java
+++ src/main/java/Moyoung/Server/chat/mapper/ChatMapper.java
@@ -1,7 +1,23 @@
 package Moyoung.Server.chat.mapper;
 
+import Moyoung.Server.chat.dto.ChatDto;
+import Moyoung.Server.chat.entity.Chat;
+import Moyoung.Server.member.entity.Member;
+import Moyoung.Server.recruitingarticle.entity.RecruitingArticle;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ChatMapper {
+    default Chat postToChat(ChatDto.Post requestBody, long memberId, long recruitArticleId) {
+        Member member = new Member();
+        member.setMemberId(memberId);
+        RecruitingArticle recruitingArticle = new RecruitingArticle();
+        recruitingArticle.setRecruitingArticleId(recruitArticleId);
+        Chat chat = new Chat();
+        chat.setSender(member);
+        chat.setRecruitingArticle(recruitingArticle);
+        chat.setContent(requestBody.getContent());
+
+        return chat;
+    }
 }
