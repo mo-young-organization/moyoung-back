8b5ddb2de3c2b498d1a967fb2e14558064caf3e3
DDunDDang <thdwns0331@gmail.com>
2023-08-03 23:12:58 +0900

    fix: 변수 추가

--- src/main/java/Moyoung/Server/recruitingarticle/entity/RecruitingArticle.java
+++ src/main/java/Moyoung/Server/recruitingarticle/entity/RecruitingArticle.java
@@ -1,5 +1,6 @@
 package Moyoung.Server.recruitingarticle.entity;
 
+import Moyoung.Server.chat.entity.Chat;
 import Moyoung.Server.member.entity.Member;
 import Moyoung.Server.runningtime.entity.RunningTime;
 import lombok.Getter;
@@ -24,8 +25,38 @@ public class RecruitingArticle {
     @ManyToOne
     @JoinColumn(name = "running_time_id")
     private RunningTime runningTime;
+    @OneToMany(mappedBy = "recruitingArticle")
+    private List<Chat> chats;
+
+    private String title;
+    // 참여 최대 인원
+    private int maxNum;
+    private Age age;
+    private Gender gender;
 
     public void addParticipant(Member member) {
         this.participants.add(member);
+    }
+    public enum Age {
+        TEENAGER("10대"),
+        TWENTIES("20대"),
+        THIRTIES("30대 이상");
+
+        @Getter
+        private String age;
+        Age(String age) {
+            this.age = age;
+        }
+    }
+    public enum Gender {
+        ALL("전체"),
+        MAN("남자만"),
+        WOMAN("여자만");
+
+        @Getter
+        private String explain;
+        Gender(String explain) {
+            this.explain = explain;
+        }
     }
 }
