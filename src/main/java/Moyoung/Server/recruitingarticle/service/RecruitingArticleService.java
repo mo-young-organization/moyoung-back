2bbd7a8b04356ef325716c07048dc14e2e8af670
DDunDDang <thdwns0331@gmail.com>
2023-08-03 23:13:45 +0900

    feat: 게시글 등록

--- src/main/java/Moyoung/Server/recruitingarticle/service/RecruitingArticleService.java
+++ src/main/java/Moyoung/Server/recruitingarticle/service/RecruitingArticleService.java
@@ -2,8 +2,12 @@ package Moyoung.Server.recruitingarticle.service;
 
 import Moyoung.Server.exception.BusinessLogicException;
 import Moyoung.Server.exception.ExceptionCode;
+import Moyoung.Server.member.entity.Member;
+import Moyoung.Server.member.service.MemberService;
 import Moyoung.Server.recruitingarticle.entity.RecruitingArticle;
 import Moyoung.Server.recruitingarticle.repository.RecruitingArticleRepository;
+import Moyoung.Server.runningtime.entity.RunningTime;
+import Moyoung.Server.runningtime.service.RunningTimeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +17,17 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class RecruitingArticleService {
     private final RecruitingArticleRepository recruitingArticleRepository;
+    private final MemberService memberService;
+    private final RunningTimeService runningTimeService;
+
+    public void registerRecruitingArticle(RecruitingArticle recruitingArticle, long memberId) {
+        Member member = memberService.findVerifiedMember(memberId);
+        RunningTime runningTime = runningTimeService.findVerifiedRunningTime(recruitingArticle.getRunningTime().getRunningTimeId());
+        recruitingArticle.setMember(member);
+        recruitingArticle.addParticipant(member);
+        recruitingArticle.setRunningTime(runningTime);
+        recruitingArticleRepository.save(recruitingArticle);
+    }
 
     public RecruitingArticle findVerifiedRecruitingArticle(long recruitingArticleId) {
         Optional<RecruitingArticle> optionalRecruitingArticle = recruitingArticleRepository.findById(recruitingArticleId);
