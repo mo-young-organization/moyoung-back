6f42aad0732c0926fd858c497ef202d679975660
DDunDDang <thdwns0331@gmail.com>
2023-08-04 15:58:36 +0900

    feat: 채팅방 불러오기

--- src/main/java/Moyoung/Server/chat/controller/ChatController.java
+++ src/main/java/Moyoung/Server/chat/controller/ChatController.java
@@ -2,18 +2,22 @@ package Moyoung.Server.chat.controller;
 
 import Moyoung.Server.auth.interceptor.JwtParseInterceptor;
 import Moyoung.Server.chat.dto.ChatDto;
+import Moyoung.Server.chat.entity.Chat;
 import Moyoung.Server.chat.mapper.ChatMapper;
 import Moyoung.Server.chat.service.ChatService;
+import Moyoung.Server.dto.MultiResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ChatController {
     private final ChatService chatService;
     private final ChatMapper chatMapper;
@@ -25,5 +29,15 @@ public class ChatController {
 
         chatService.SendChat(chatMapper.postToChat(requestBody, authenticationMemberId, recruitArticleId));
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/recruit/{recruit-id}/chat")
+    public ResponseEntity getChat(@PathVariable("recruit-id") long recruitArticleId,
+                                  @RequestParam int page){
+        long authenticationMemberId = JwtParseInterceptor.getAuthenticatedMemberId();
+        Page<Chat> pageChat = chatService.loadChat(recruitArticleId, authenticationMemberId, page);
+        List<Chat> chatList = pageChat.getContent();
+
+        return new ResponseEntity<>(new MultiResponseDto<>(chatMapper.chatsToList(chatList), pageChat), HttpStatus.OK);
     }
 }
--- src/main/java/Moyoung/Server/chat/repository/ChatRepository.java
+++ src/main/java/Moyoung/Server/chat/repository/ChatRepository.java
@@ -1,7 +1,10 @@
 package Moyoung.Server.chat.repository;
 
 import Moyoung.Server.chat.entity.Chat;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+    Page<Chat> findAllByRecruitingArticleRecruitingArticleId(long recruitingArticleId, Pageable pageable);
 }
