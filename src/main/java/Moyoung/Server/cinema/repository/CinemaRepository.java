0963e068e06b493b9f3a1ce3ca1fd8624c348d3a
DDunDDang <thdwns0331@gmail.com>
2023-09-10 21:50:48 +0900

    feat: 위도 경도를 통해 찾는 쿼리 작성

--- src/main/java/Moyoung/Server/cinema/repository/CinemaRepository.java
+++ src/main/java/Moyoung/Server/cinema/repository/CinemaRepository.java
@@ -2,9 +2,14 @@ package Moyoung.Server.cinema.repository;
 
 import Moyoung.Server.cinema.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
     List<Cinema> findAllByBrand(String brand);
+
+    @Query("SELECT c FROM Cinema c WHERE ST_DISTANCE_SPHERE(POINT(c.longitude, c.latitude), POINT(:longitude, :latitude)) <= :distance")
+    List<Cinema> findCinemasWithinDistance(@Param("latitude") double latitude, @Param("longitude") double longitude, @Param("distance") double distance);
 }
