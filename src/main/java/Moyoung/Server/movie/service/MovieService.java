f498bd096bb19dd8fbe0cea5739717f1e40e2b11
DDunDDang <thdwns0331@gmail.com>
2023-09-10 21:52:29 +0900

    feat: 식별자를 통한 영화 찾기

--- src/main/java/Moyoung/Server/movie/service/MovieService.java
+++ src/main/java/Moyoung/Server/movie/service/MovieService.java
@@ -1,11 +1,27 @@
 package Moyoung.Server.movie.service;
 
+import Moyoung.Server.exception.BusinessLogicException;
+import Moyoung.Server.exception.ExceptionCode;
+import Moyoung.Server.movie.entity.Movie;
 import Moyoung.Server.movie.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MovieService {
-    private MovieRepository movieRepository;
+    private final MovieRepository movieRepository;
+
+    public Movie findMovie(long movieId) {
+        return findVerifiedMovie(movieId);
+    }
+
+    private Movie findVerifiedMovie(long movieId) {
+        Optional<Movie> optionalMovie = movieRepository.findById(movieId);
+
+        return optionalMovie.orElseThrow(() ->
+                new BusinessLogicException(ExceptionCode.MOVIE_NOT_FOUND));
+    }
 }
