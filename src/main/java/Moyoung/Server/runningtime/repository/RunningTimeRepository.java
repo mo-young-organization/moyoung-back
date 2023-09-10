937b17f70c6562dea3e721173ee4474a66a94d57
DDunDDang <thdwns0331@gmail.com>
2023-09-10 21:53:29 +0900

    feat: 상영정보 조회 쿼리 추가

--- src/main/java/Moyoung/Server/runningtime/repository/RunningTimeRepository.java
+++ src/main/java/Moyoung/Server/runningtime/repository/RunningTimeRepository.java
@@ -1,7 +1,13 @@
 package Moyoung.Server.runningtime.repository;
 
+import Moyoung.Server.cinema.entity.Cinema;
+import Moyoung.Server.movie.entity.Movie;
 import Moyoung.Server.runningtime.entity.RunningTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface RunningTimeRepository extends JpaRepository<RunningTime, Long> {
+    List<RunningTime> findRunningTimesByCinemaAndMovieAndStartTimeBetween(Cinema cinema, Movie movie, LocalDateTime startOfDate, LocalDateTime endOfDate);
 }
