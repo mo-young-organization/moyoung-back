de54a2efb4922d22365413518a559cc9238d0807
DDunDDang <thdwns0331@gmail.com>
2023-09-14 15:38:19 +0900

    feat: 영화 제목(일부분 가능)으로 영화찾기

--- src/main/java/Moyoung/Server/movie/dto/MovieDto.java
+++ src/main/java/Moyoung/Server/movie/dto/MovieDto.java
@@ -1,4 +1,24 @@
 package Moyoung.Server.movie.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
 public class MovieDto {
+    @Getter
+    @Builder
+    public static class Response {
+        private long movieId;
+        private String name;
+        private String thumbnailUrl;
+    }
+
+    @Getter
+    @Builder
+    public static class NearResponse {
+        private long movieId;
+        private String name;
+        private String thumbnailUrl;
+        private String movieRating;
+        private String info;
+    }
 }
--- src/main/java/Moyoung/Server/movie/service/MovieService.java
+++ src/main/java/Moyoung/Server/movie/service/MovieService.java
@@ -1,11 +1,35 @@
 package Moyoung.Server.movie.service;
 
+import Moyoung.Server.exception.BusinessLogicException;
+import Moyoung.Server.exception.ExceptionCode;
+import Moyoung.Server.movie.entity.Movie;
 import Moyoung.Server.movie.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
-    private MovieRepository movieRepository;
+    private final MovieRepository movieRepository;
+
+    public Movie findMovie(long movieId) {
+        return findVerifiedMovie(movieId);
+    }
+
+    public Page<Movie> findMoviesByName(String movieName, Integer page) {
+        if (page == null) page = 1;
+        return movieRepository.findAllByNameContains(movieName, PageRequest.of(page - 1, 25, Sort.by("movieId").descending()));
+    }
+
+    private Movie findVerifiedMovie(long movieId) {
+        Optional<Movie> optionalMovie = movieRepository.findById(movieId);
+
+        return optionalMovie.orElseThrow(() ->
+                new BusinessLogicException(ExceptionCode.MOVIE_NOT_FOUND));
+    }
 }
