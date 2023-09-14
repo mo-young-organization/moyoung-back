a9fbc9a9cec404fb9705a76556bf26379015832e
DDunDDang <thdwns0331@gmail.com>
2023-09-14 15:37:19 +0900

    fix: 오류 메세지 추가

--- src/main/java/Moyoung/Server/exception/ExceptionCode.java
+++ src/main/java/Moyoung/Server/exception/ExceptionCode.java
@@ -6,6 +6,7 @@ public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "회원을 찾을 수 없습니다"),
     MEMBER_ID_EXISTS(409, "현재 사용중인 아이디 입니다."),
     RECRUIT_ARTICLE_NOT_FOUND(404, "게시글을 찾을 수 없습니다"),
+    MOVIE_NOT_FOUND(404, "영화를 찾을 수 없습니다"),
     UNAUTHORIZED(404, "권한이 없습니다" );
     @Getter
     private int status;
