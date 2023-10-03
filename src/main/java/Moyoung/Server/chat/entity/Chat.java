50f1ef533fd69715018b6b721dbf35b10f7cb6e0
DDunDDang <thdwns0331@gmail.com>
2023-10-03 16:39:43 +0900

    fix: chat entity 수정

--- src/main/java/Moyoung/Server/chat/entity/Chat.java
+++ src/main/java/Moyoung/Server/chat/entity/Chat.java
@@ -2,25 +2,33 @@ package Moyoung.Server.chat.entity;
 
 import Moyoung.Server.member.entity.Member;
 import Moyoung.Server.recruitingarticle.entity.RecruitingArticle;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Chat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long chatId;
-    private LocalDateTime chatTime;
+    @Enumerated(value = EnumType.STRING)
+    private ContentType type;
+    private LocalDateTime createdAt;
     private String content;
+    private String sender;
+    private int unreadCount;
 
-    @ManyToOne
-    @JoinColumn(name = "member_id")
-    private Member sender;
     @ManyToOne
     @JoinColumn(name = "recruiting_article_id")
     private RecruitingArticle recruitingArticle;
+
+    public enum ContentType {
+        ENTER, TALK, LEAVE, REENTER, NOTICE, DISCONNECTED, BAN
+    }
+
 }
