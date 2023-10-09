0bb18d6b07f2d9e14a9f1ac47b262f0097234565
DDunDDang <thdwns0331@gmail.com>
2023-10-09 22:19:08 +0900

    feat: 토큰 검증

--- src/main/java/Moyoung/Server/auth/interceptor/JwtParseInterceptor.java
+++ src/main/java/Moyoung/Server/auth/interceptor/JwtParseInterceptor.java
@@ -17,37 +17,49 @@ import java.util.Map;
 @Component
 public class JwtParseInterceptor implements HandlerInterceptor {
     private final JwtUtils jwtUtils;
-    private static final ThreadLocal<Long> authenicatedMemberId = new ThreadLocal<>();
+    private static final ThreadLocal<Long> authenticatedMemberId = new ThreadLocal<>();
 
     public JwtParseInterceptor(JwtUtils jwtUtils) {
         this.jwtUtils = jwtUtils;
     }
 
     public static long getAuthenticatedMemberId() {
-        return authenicatedMemberId.get();
+        return authenticatedMemberId.get();
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         try {
+            String authorizationHeader = request.getHeader("Authorization");
+            if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+                // 유효하지 않은 토큰 처리
+                ErrorResponder.sendErrorResponse(response, HttpStatus.UNAUTHORIZED);
+                return false;
+            }
+
+            String jws = authorizationHeader.substring(7);
+            // 토큰 검증
+            jwtUtils.validateToken(jws);
+
             Map<String, Object> claims = jwtUtils.getJwsClaimsFromRequest(request);
             Object memberId = claims.get("memberId");
             if (memberId != null) {
-                authenicatedMemberId.set(Long.valueOf(memberId.toString()));
+                authenticatedMemberId.set(Long.valueOf(memberId.toString()));
                 return true;
             } else {
-                // memberId가 null인 경우에 대한 예외 처리
+                // memberId가 null 인 경우에 대한 예외 처리
                 ErrorResponder.sendErrorResponse(response, HttpStatus.UNAUTHORIZED);
                 return false;
             }
         } catch (Exception e) {
-            authenicatedMemberId.set(-1L);  // 유효하지 않는 id 값을 설정하여 로그인이 필요한 기능 호출 시 진행되지 않도록 함
-            return true;
+            // 예외 처리
+            ErrorResponder.sendErrorResponse(response, HttpStatus.UNAUTHORIZED);
+            return false;
         }
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
-        authenicatedMemberId.remove();
+        authenticatedMemberId.remove();
     }
 }
