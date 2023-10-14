fc949e4d6b3dc42f4344ae4dad12839f4d54c3c5
DDunDDang <thdwns0331@gmail.com>
2023-10-15 00:40:49 +0900

    fix: 이름 변경 및 websocket 연결 설정

--- src/main/java/Moyoung/Server/config/WebSocketConfig.java
+++ src/main/java/Moyoung/Server/config/WebSocketConfig.java
@@ -1,20 +1,32 @@
 package Moyoung.Server.config;
 
-import Moyoung.Server.chat.handler.ChatHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocket
+@EnableWebSocketMessageBroker
 @RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
-    private final ChatHandler chatHandler;
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatHandler, "/ws/chat").setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 메세지 구독 요청 url -> 메세지 받을 때
+        registry.enableSimpleBroker("/sub");
+        // 메세지 발행 요청 url -> 메세지 보낼 때
+        registry.setApplicationDestinationPrefixes("/pub");
     }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("https://apic.app");//.withSockJS();
+//        registry.addEndpoint("/ws").setAllowedOrigins("*")
+//                .withSockJS();
+    }
+
+//    @Override
+//    public void configureClientInboundChannel(ChannelRegistration registration){
+//        registration.interceptors(stompHandler);
+//    }
 }
