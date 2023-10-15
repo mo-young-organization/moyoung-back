9fe15427d250cfec48e3e5abda1a35eda0abcc2c
DDunDDang <thdwns0331@gmail.com>
2023-10-15 10:52:41 +0900

    fix: WS 연결 시 token 검증 로직 적용

--- src/main/java/Moyoung/Server/config/WebSocketConfig.java
+++ src/main/java/Moyoung/Server/config/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package Moyoung.Server.config;
 
+import Moyoung.Server.StompHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
@@ -9,7 +11,7 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+    private final StompHandler stompHandler;
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // 메세지 구독 요청 url -> 메세지 받을 때
@@ -25,8 +27,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 //                .withSockJS();
     }
 
-//    @Override
-//    public void configureClientInboundChannel(ChannelRegistration registration){
-//        registration.interceptors(stompHandler);
-//    }
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration){
+        registration.interceptors(stompHandler);
+    }
 }
