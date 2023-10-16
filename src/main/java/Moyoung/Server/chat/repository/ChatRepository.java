9a2c3ecf02ffa7822a4db65cb4d12b28a805ab9b
DDunDDang <thdwns0331@gmail.com>
2023-10-16 17:17:24 +0900

    fix: 기존코드 제거 및 참여 시간 이후의 채팅을 받아올 수 있도록 변경

--- src/main/java/Moyoung/Server/chat/repository/ChatRepository.java
+++ src/main/java/Moyoung/Server/chat/repository/ChatRepository.java
@@ -1,13 +1,13 @@
 package Moyoung.Server.chat.repository;
 
 import Moyoung.Server.chat.entity.Chat;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-    Page<Chat> findAllByRecruitingArticleRecruitingArticleId(long recruitingArticleId, Pageable pageable);
-    List<Chat> findAllByRecruitingArticleRecruitingArticleId(long recruitingArticleId);
+    @Query("SELECT c FROM Chat c WHERE c.recruitingArticle.recruitingArticleId = :recruitingArticleId AND c.chatTime > :entryDate")
+    List<Chat> findChatsByRecruitingArticleAndEntryDate(long recruitingArticleId, LocalDateTime entryDate);
 }
