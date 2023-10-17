765892216d528dfe059fd9a59e7faf0a2fc60034
DDunDDang <thdwns0331@gmail.com>
2023-10-17 16:28:47 +0900

    feat: Timezone 설정

--- src/main/java/Moyoung/Server/ServerApplication.java
+++ src/main/java/Moyoung/Server/ServerApplication.java
@@ -3,8 +3,15 @@ package Moyoung.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ServerApplication {
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
