9695a6ea1aefc6f3066aba2df7692417f70d1f72
DDunDDang <thdwns0331@gmail.com>
2023-10-23 13:16:49 +0900

    remove: 영화 크롤러 엔티티 제거

--- src/main/java/Moyoung/Server/crawler/repository/Cinema_crawlerRepository.java
+++
@@ -1,7 +0,0 @@
-package Moyoung.Server.crawler.repository;
-
-import Moyoung.Server.crawler.entity.Cinema_crawler;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Cinema_crawlerRepository extends JpaRepository<Cinema_crawler, Long> {
-}
