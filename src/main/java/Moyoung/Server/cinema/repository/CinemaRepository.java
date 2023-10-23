c665833ef7acfc92233208e1496addb241dc05bf
DDunDDang <thdwns0331@gmail.com>
2023-10-23 13:22:47 +0900

    fix: 영화관 필터링 로직 수정

--- src/main/java/Moyoung/Server/cinema/repository/CinemaRepository.java
+++ src/main/java/Moyoung/Server/cinema/repository/CinemaRepository.java
@@ -6,16 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Set;
 
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
-    List<Cinema> findAllByBrand(String brand);
-
-@Query("SELECT c FROM Cinema c WHERE ST_DISTANCE_SPHERE(POINT(c.y, c.x), POINT(:longitude, :latitude)) <= :distance " +
-        "AND ((:mega = true AND c.brand = 'mega') OR (:lotte = true AND c.brand = 'lotte') OR (:cgv = true AND c.brand = 'cgv') OR (:mega = false AND :lotte = false AND :cgv = false))")
-List<Cinema> findCinemasWithinDistanceAndFilter(@Param("latitude") double latitude,
-                                                @Param("longitude") double longitude,
-                                                @Param("distance") double distance,
-                                                @Param("mega") boolean mega,
-                                                @Param("lotte") boolean lotte,
-                                                @Param("cgv") boolean cgv);
+    @Query("SELECT c FROM Cinema c WHERE ST_DISTANCE_SPHERE(POINT(c.x, c.y), POINT(:longitude, :latitude)) <= :distance " +
+            "AND (c.brand IN :brands) " +
+            "ORDER BY ST_DISTANCE_SPHERE(POINT(c.x, c.y), POINT(:longitude, :latitude))")
+    List<Cinema> findCinemasWithinDistanceAndFilter(@Param("latitude") double latitude,
+                                                    @Param("longitude") double longitude,
+                                                    @Param("distance") double distance,
+                                                    @Param("brands") Set<String> brands);
 }
