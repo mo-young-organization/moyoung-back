5253e61ede9c24f37990da51463863abdc15c643
DDunDDang <thdwns0331@gmail.com>
2023-12-01 21:43:47 +0900

    test: 비로그인 게시물 url 추가

--- src/test/java/Moyoung/Server/helper/RecruitingArticleControllerHelper.java
+++ src/test/java/Moyoung/Server/helper/RecruitingArticleControllerHelper.java
@@ -4,4 +4,5 @@ public interface RecruitingArticleControllerHelper extends ControllerHelper {
     String RECRUITING_ARTICLE_URL = "/recruit";
     String RECRUITING_ARTICLE_RESOURCE_ID = "/{recruit-id}";
     String RECRUITING_ARTICLE_RESOURCE_URI = RECRUITING_ARTICLE_URL + RECRUITING_ARTICLE_RESOURCE_ID;
+    String RECRUITING_ARTICLE_NON_LOGIN_GET_URL = "/recruit-article";
 }
